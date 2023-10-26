e62972546920db8ed44892fa061a398819356c5a
Stephan B Valois <83061036+ScopeSV@users.noreply.github.com>
2023-10-26 12:51:32 +0200

    help text

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
+#!/usr/bin/env node
 import { Command } from 'commander'
 import createMigrationHandler from './handlers/migrationHandler'
 
 const program = new Command()
 
 program.name('kysely-cli').description('Cli tool for kysely').version('0.0.1')
+
+program
+    .command('migration')
+    .description('Manage migrations')
+    .on('--help', () => {
+        console.log(`
+    MAKE:
+    To create a new table:
+    $ kycli migration:make create_foo_table foo:string bar:integer
+
+    To add fields to an existing table:
+    $ kycli migration:make add_foo_from_foo_table foo:string bar:integer
+
+    To remove fields from an existing table:
+    $ kycli migration:make remove_foo_from_footable foo
+        `)
+    })
+    .action((cmdObj) => {
+        if (cmdObj.args.length === 0) {
+            cmdObj.outputHelp()
+            process.exit(1)
+        }
+    })
 
 program
     .command('migration:make')
